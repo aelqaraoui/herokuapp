321efea9c584b3e9f584c2970ab1a69a7fecc22d
Amine <aelqaraoui101@gmail.com>
2021-09-09 09:27:47 +0100

    F

--- server.js
+++ server.js
@@ -13,10 +13,67 @@ app.use(cors())
 
 const owner = "CyBizpsEVPjycYiaCMaDQFkHJWrPZJsBYWeYTz3JYVPX";
 
+const wl = ['http://localhost:3000', 'http://localhost:8080', 'https://sleepy-atoll-58033.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (wl.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+const startWhitelist = (num) => {
+    let whitelist = fs.readFileSync('whitelist', 'utf-8').split('\n');
+    whitelist.push(owner);
+
+    let mintsLeftDB = {};
+    whitelist.forEach(x => {
+        mintsLeftDB[x] = num;
+    });
+    fs.writeFileSync('mintsLeftDB.json', JSON.stringify(mintsLeftDB));
+    fs.writeFileSync('transactionsDB.json', JSON.stringify({}));
+}
+
+const registerMint = (num, address, signature) => {
+    let mintsLeftDB = JSON.parse(fs.readFileSync('mintsLeftDB.json', 'utf-8'));
+    let transactionsDB = JSON.parse(fs.readFileSync('transactionsDB.json', 'utf-8'));
+
+    mintsLeftDB[address] -= num;
+    transactionsDB[address] = {signature, num_minted: num};
+
+    fs.writeFileSync('mintsLeftDB.json', JSON.stringify(mintsLeftDB));
+    fs.writeFileSync('transactionsDB.json', JSON.stringify(transactionsDB));
+}
+
+app.post('/getMintsLeft', (req, res) => {
+    let address = req.body.address;
+    let mintsLeftDB = JSON.parse(fs.readFileSync('mintsLeftDB.json', 'utf-8'));
+    
+    res.send({mints_left: mintsLeftDB[address]})
+})
+
+app.post('/registerMint', (req, res) => {
+    let {num, address, signature} = req.body;
+    
+    registerMint(num, address, signature);
+
+    res.end();
+})
+
+app.get('/getTransactions', (req, res) => {
+    
+    res.send(JSON.parse(fs.readFileSync('transactionsDB.json', 'utf-8')));
+})
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
